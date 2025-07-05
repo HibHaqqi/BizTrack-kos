b14c0ffc14099d4cec3bb5582e942383a7feaaeb
hibhaqqi <haqqi09@gmail.com>
2025-07-06 00:44:47 +0800

    page

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-4xl font-bold mb-4 text-blue-600">Customers Page</h1>
+      <p className="text-lg text-gray-700">This is a simple customers page using Next.js & Tailwind CSS.</p>
+    </div>
+  );
+}
